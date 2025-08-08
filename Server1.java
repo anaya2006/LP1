a53c325a68798c4123331bee3cf485c505462df2
anaya2006 <anayabadave@gmail.com>
2025-08-08 13:46:17 +0530

    Create Server1.java

---
+++ Server1.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientHandler implements Runnable {
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+            Server.broadcastMessage("SERVER: " + clientUsername + " has entered the chat!", this);
+        } catch (IOException e) {
+            closeEverything();
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = bufferedReader.readLine();
+                if (messageFromClient == null) break;
+                Server.broadcastMessage(messageFromClient, this);
+            } catch (IOException e) {
+                closeEverything();
+                break;
+            }
+        }
+    }
+
+    public void sendMessage(String message) throws IOException {
+        bufferedWriter.write(message);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+    }
+
+    public void closeEverything() {
+        Server.removeClientHandler(this);
+        try {
+            if (bufferedReader != null) bufferedReader.close();
+            if (bufferedWriter != null) bufferedWriter.close();
+            if (socket != null) socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
